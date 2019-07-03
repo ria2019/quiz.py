94d6673feaaa33c9a586c19b3f9431cdbad0b5f5
ria2019 <52420258+ria2019@users.noreply.github.com>
2019-07-03 15:35:06 -0500

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,10 @@
+#quiz
+x = int(input("Putfirst"))
+y = int(input("Putfirst"))
+z = int(input("Putfirst"))
+square = x * x
+y = y/2
+square = square- y
+square = square * 3
+#{(X2-y/z) * 3
+print(square)
